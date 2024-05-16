f4adfd288345c4539f5d96b86b647e3053cec6aa
Michael Hensley <29671149+ZRexshima@users.noreply.github.com>
2024-05-17 08:06:34 +0900

    initial commit

---
+++ table.py
@@ -0,0 +1,22 @@
+from fpdf import FPDF
+
+pdf = FPDF()
+pdf.set_font("Helvetica")
+pdf.set_display_mode(zoom="fullwidth")
+
+TABLE_DATA = (
+    ("First name", "Last name", "Age", "City"),
+    ( "Jules", "Smith", "34", "San Juan"),
+    ( "Mary", "Ramos", "45", "Orlando"),
+    ("Carlson", "Banks", "19", "Los Angeles"),
+    ("Lucas", "Cimon", "31", "Saint-Mathurin-sur-Loire"),
+)
+
+pdf.add_page()
+pdf.set_font("Times", size=16)
+with pdf.table() as table:
+    for data_row in TABLE_DATA:
+        row = table.row()
+        for datum in data_row:
+            row.cell(datum)
+pdf.output("table.pdf")
